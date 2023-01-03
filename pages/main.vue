495486f122abc227bc6792698bd12c67432f7f67
Dongmin Kim <qlsnaos12@naver.com>
2023-01-03 23:15:42 +0900

    add a backend, setting of server

--- pages/main.vue
+++ pages/main.vue
@@ -1,14 +1,27 @@
 <template>
   <div id="app">
     <p>메인 페이지입니다.</p>
+    <ul>
+      <li v-for="item in this.products" :key="item">{{ item }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-
+  data(){
+    return{
+      products : [],
+    }
+  },
+  async created(){
+    const response = await axios.get('http://localhost:3000/products');
+    this.products = response.data;
+  }
 }
-</script>
+</script> 
+
 
 <style>
 
